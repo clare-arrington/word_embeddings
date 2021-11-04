0658369d80fb86e406a9f209071fe2be3c543c1c
Clare Arrington <arrinj@rpi.edu>
2021-11-04 12:09:10 -0400

    Sense and target split

--- pick_targets.py
+++ pick_targets.py
@@ -1,3 +1,4 @@
+#%%
 from gensim.models import Word2Vec
 import matplotlib.pyplot as plt
 import numpy
@@ -6,21 +7,84 @@ path = f'/home/clare/Data/word_vectors/semeval/sense/ccoha1.vec'
 model = Word2Vec.load(path)
 vocab = list(model.wv.index_to_key)
 
-lens = []
-counts = []
-targets = []
+targets = ['little', 'much', 'hand', 'long', 
+            'look', 'nul', 'shall', 'first', 
+            'good', 'place', 'two', 'life', 
+            'old', 'never', 'without', 'yet', 
+            'many', 'heart', 'might', 'thing', 
+            'leave', 'seem', 'love', 'power', 
+            'feel', 'though', 'far', 'country', 
+            'way', 'mind', 'tell', 'work', 
+            'still', 'hear', 'call', 'people', 
+            'form', 'house', 'friend', 'young', 
+            'stand', 'speak', 'last', 'world', 
+            'ever', 'get', 'present', 'whole', 
+            'right', 'pass', 'high', 'let', 
+            'god', 'become', 'child', 'bring', 
+            'another', 'father', 'light', 
+            'among', 'law', 'mean', 'fall', 
+            'turn', 'name', 'nothing', 'whose', 
+            'moment', 'general', 'side', 
+            'nature', 'away', 'hope', 'use', 
+            'subject', 'spirit', 'thy', 
+            'character', 'however', 'three', 
+            'large', 'keep', 'soon', 'return', 
+            'live', 'night', 'hold', 'government', 
+            'back', 'person', 'case', 'put', 
+            'lay', 'believe', 'hour', 'point', 
+            'foot', 'woman', 'sir', 'true', 
+            'water', 'cause', 'mother', 'less',
+            'always', 'receive', 'course', 
+            'home', 'better', 'half', 'order',
+            'death', 'arm', 'manner', 'small',
+            'within', 'almost', 'follow', 'lady',
+            'open', 'voice', 'public', 'meet',
+            'party', 'truth', 'want', 'fact', 
+            'soul', 'poor', 'object']
+
+# lens = []
+# counts = []
+# targets = []
+normal_len = []
+normal_count = []
+target_len = []
+target_count = []
+sense_len = []
+sense_count = []
 for word in vocab:
     count = model.wv.get_vecattr(word, 'count')
     vec_len = numpy.linalg.norm(model.wv[word])
-    
-    if count in range(2500, 7500) and int(vec_len) in range(7, 15):
-        counts.append(count)
-        lens.append(vec_len)
-        targets.append(word)
-        
-print(len(counts))
-    
-plt.scatter(counts, lens)
-plt.savefig('test.png')
 
-print(targets)
+    if word in targets:
+        # color.append('firebrick')
+        target_count.append(count)
+        target_len.append(vec_len)
+    elif '.' in word:
+        # color.append('firebrick')
+        sense_count.append(count)
+        sense_len.append(vec_len)
+    else:
+        # color.append('steelblue')
+        normal_count.append(count)
+        normal_len.append(vec_len)
+    
+    # if count in range(2500, 7500) and int(vec_len) in range(7, 15):
+
+        # targets.append(word)
+        
+# print(len(counts))
+
+#%%
+plt.figure(figsize=(8, 6), dpi=80)
+plt.scatter(normal_count, normal_len, c='darkgrey')
+plt.scatter(target_count, target_len, c='firebrick', label='target')
+plt.scatter(sense_count, sense_len, c='teal', label='sense')
+plt.xlabel('Term Frequency')
+plt.ylabel('Term Vector Length')
+plt.legend()
+plt.show()
+# plt.savefig('test.png')
+
+# print(targets)
+
+# %%
