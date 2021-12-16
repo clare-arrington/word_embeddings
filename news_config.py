9d2fb7a1d5d2603e9dd764bf02fa1460c79ecf7c
Clare Arrington <arrinj@rpi.edu>
2021-12-16 14:53:20 -0500

    Restructure into a function

--- news_config.py
+++ news_config.py
@@ -1,31 +1,40 @@
 #%%
 from base_wv import main, make_config
+import itertools
 
-dataset = "news"
-run = "sense"
-data_path = '/data/arrinj'
+def make_wv(corpus_name, run, dataset="news", data_path="/data/arrinj"):
+    min_count = 45
+    vector_size = 300
 
-min_count = 25
-vector_size = 300
-load_data = False
-save_data = True
+    if run == 'new':
+        load_data = False
+        save_data = True
+    else:
+        load_data = True
+        save_data = False
 
-with open(f'{data_path}/corpus_data/{dataset}/targets.txt') as fin:
-    targets = fin.read().split()
+    with open(f'{data_path}/corpus_data/{dataset}/targets.txt') as fin:
+        targets = fin.read().split()
 
-# corpus_name = "alternative"
-# slice_num = 0
+    paths = {
+            'corpus_path': f'corpus_data/{dataset}/subset/{corpus_name}/',
+            'target_path': f'corpus_data/{dataset}/subset/{corpus_name}/'
+        }
 
-for corpus_name in ['alternative', 'mainstream']:
-    for slice_num in range(0, 6):
+    wv_config = make_config(
+            dataset, corpus_name, run, min_count, 
+            vector_size, targets, load_data, save_data, 
+            data_path, paths)
 
-        config = make_config(
-                dataset, corpus_name, run, min_count, 
-                vector_size, targets, load_data, save_data, data_path, slice_num)
-        
-        main(config)
+    main(wv_config)
+    print('\n\n')
+    
+corpora = ['conspiracy', 'mainstream'] # , 
+run_types = ['new', 'sense']
 
-        print('\n\n')
+for corpus_name, run in itertools.product(corpora, run_types):
+    print(f'\n\n\n======= Making {corpus_name} {run} wordvector =======')
+    make_wv(corpus_name, run)
 
 print('Done!')
 
